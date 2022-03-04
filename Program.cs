02050608b6939c1c7d5b5c279449c8c2ba3a6964
wagnerdsodre <wagnerdsodre@gmail.com>
2022-03-03 21:29:54 -0300

    File CreateText

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿
+//Criando um path combinado
+//var path = Path.Combine(Environment.CurrentDirectory , "file.txt");
+var path = Path.Combine(@"c:\temp" , "file.txt");
+
+
+var fc = File.CreateText(path);
+fc.WriteLine("Este é um arquivo de texto da linha 1");
+fc.WriteLine("Este é um arquivo de texto da linha 2");
+fc.WriteLine("Este é um arquivo de texto da linha 3");
+fc.WriteLine("Este é um arquivo de texto da linha 4");
+fc.Flush();
+
